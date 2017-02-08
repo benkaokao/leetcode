cd78e24db7e97cb5772fa55f450f067e2dc50b62
j06188 <j06188@gmail.com>
2017-02-08 19:04:14 +0800

    Create 3. LongestSubstringWithoutRepeatingCharacters.py

---
+++ 3. LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        start=0
+        mexlen=0
+        drop={}
+        for i in range(len(s)):
+            #查看s[i]是否在字典中而且指針在s[i]的後面
+            if s[i] in drop :
+                start=drop[s[i]]+1
+            else:
+                mexlen=max(mexlen,i+1-start) 
+            drop[s[i]]=i
+        return mexlen
