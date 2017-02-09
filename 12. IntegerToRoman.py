97cb5d0f7431a8c9905db327f6f79983e2ae7bcc
j06188 <j06188@gmail.com>
2017-02-09 21:26:44 +0800

    Create 12. IntegerToRoman.py

---
+++ 12. IntegerToRoman.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        I=['','I','II','III','IV','V','VI','VII','VIII','IX']
+        X=['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+        C=['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
+        M=['','M','MM','MMM']
+        if num>0 and num<4000:
+            ans=M[num/1000]+C[(num%1000)/100]+X[(num%100)/10]+I[num%10]
+        return ans
